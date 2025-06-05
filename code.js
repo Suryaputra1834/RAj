f69d0855d8ebe4df8d6fb1906ee4f63890402917
Suryaputra1834 <i1213raj@email.com>
2025-06-05 13:27:28 +0530

    learning

---
+++ code.js
@@ -0,0 +1,18 @@
+let i=2;
+if(i%2==0){
+    console.log(" the given number is Even");
+}
+else
+{
+    console.log("the given number is Odd");
+}
+console.log("even numbers")
+for(let j=0;j<=10;j=j+2){
+    console.log(j);
+}
+console.log("odd numbers");
+for(let k=0;k<=10;k++){
+    if(k%2!=0){
+        console.log(k);
+    }
+}
